3d2f043658dade48c8d1d14c392b193cd9fcbe3f
Mikolaj <mgafiuk2001@wp.pl>
2025-02-11 20:51:36 +0100

    Gotowy

---
+++ JAVA/main.js
@@ -0,0 +1,22 @@
+// Standard ES5
+
+function iloczyn(num1, num2, num3) {
+    let result = num1 * num2 * num3;
+
+    return result
+}
+
+
+let wynikIloczynu = iloczyn(2, 3, 4);
+
+console.log(wynikIloczynu);
+
+// Standard ES6
+
+const iloczyn2 (num1, num2, num3) => 
+    num1 * num2 * num3
+
+
+let wynikIloczynu2 = iloczyn2(2, 3, 4);
+
+console.log(wynikIloczynu2);
